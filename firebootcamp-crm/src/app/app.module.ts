3fd79dbd244b2a4531ceffc790075573eb670208
JeanThirion <jean.thirion@esial.net>
2018-03-08 12:39:27 +1100

    company list

--- firebootcamp-crm/src/app/app.module.ts
+++ firebootcamp-crm/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { CompanyListComponent } from './company/company-list/company-list.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { CompanyListComponent } from './company/company-list/company-list.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
